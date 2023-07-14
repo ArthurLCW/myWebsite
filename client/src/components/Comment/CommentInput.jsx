3a35de6da4686c2115830ca641acc15620c136b1
Changwenl <arthurlicw@gmail.com>
2023-07-14 17:50:23 +0800

    add rich text editor

--- client/src/components/Comment/CommentInput.jsx
+++ client/src/components/Comment/CommentInput.jsx
@@ -1,45 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./CommentInput.scss"
+import Vditor from "vditor";
+import "vditor/dist/index.css";
+
+
 import { Avatar, Button, Icon, SvgIcon } from '@material-ui/core'
 import SendIcon from '@material-ui/icons/Send';
-import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
 
 const CommentInput = ({blogName, commentType}) => {
   let url = process.env.REACT_APP_PROTOCOL+"://"+process.env.REACT_APP_IP+":"+process.env.REACT_APP_BACKEND_PORT;
-  const [post, setPost] = useState({
-    username: "Anonymous",
-    postname: blogName,
-    comment: "",
-  });
-  const [error, setError] = useState(false)
+  const [username, setUsername] = useState('Visitor');
+  const [vd, setVd] = useState();
 
-  const handleChange = (e) => {
-    setPost((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+  useEffect(() => {
+    const vditor = new Vditor("vditor", {
+      height: '30vh',
+      after: () => {
+        vditor.setValue("");
+        setVd(vditor);
+      }
+    });
+  }, []);
 
   const handleClick = async (e) => {
     e.preventDefault();
-    var comment = document.getElementById("comment").value;
-    if (comment.length==0){
+    let comment = vd.getValue();
+    console.log('Value:', JSON.stringify(comment)); // Use JSON.stringify to reveal whitespace characters
+    console.log('Length:', comment.length);
+    if (comment.length==1){
       alert("Your comment is EMPTY.");
       return null;
     }
 
     try {
+      let post = {
+        username: username,
+        postname: blogName,
+        comment: comment,
+      }
       console.log("try: ", post);
-      // await axios.post("http://localhost:8800/api/comment", post); ///////////////////////////////
-      // await axios.post("https://changwenli.com:8801/api/comment", post);
       console.log("frontend connected to url: ", url+"/api/comment");
       await axios.post(url+"/api/comment", post);
-      // navigate("/");
-      // document.getElementById("myTextarea").value = "";
+      vd.setValue('')
 
     } catch (err) {
       console.log(err);
+      alert('Error: '+err);
     }
   };
 
@@ -48,12 +58,8 @@ const CommentInput = ({blogName, commentType}) => {
     <div className="comment-container">
       <br/>
       <h3>{commentType}</h3>
-      <textarea id="comment" className="comment" rows={5} 
-      onChange={handleChange} name="comment">
-      </textarea>
-
+      <div id="vditor" className="vditor" />
       <div className="toolbar">
-        <SvgIcon component={EmojiEmotionsIcon}/>
         <SvgIcon component={SendIcon} onClick={handleClick}/>
       </div>
     </div>
